31935719fe0a2da599cf85a558988aace26125a9
zunairadabeer <zunairadabeer@gmail.com>
2026-06-18 17:46:11 +0100

    Added sample inventory dataset

---
+++ sample_data.py
@@ -0,0 +1,33 @@
+from inventory import add_item
+
+def load_sample_inventory():
+
+    add_item(
+        "chicken",
+        2,
+        "2026-06-20"
+    )
+
+    add_item(
+        "pasta",
+        1,
+        "2026-07-10"
+    )
+
+    add_item(
+        "egg",
+        6,
+        "2026-06-19"
+    )
+
+    add_item(
+        "cheese",
+        1,
+        "2026-06-22"
+    )
+
+    add_item(
+        "broccoli",
+        1,
+        "2026-06-18"
+    )
